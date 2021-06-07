0a48b8bed4bd89181ecf1417924f22dd50857b07
Kyle Awayan <56655579+kyleawayan@users.noreply.github.com>
2021-06-06 22:27:25 -0400

    add opengraph/seo info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
-import Head from "next/head";
+import { NextSeo } from "next-seo";
 import LogoAnimated from "../components/LogoAnimated";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Zuyu</title>
-      </Head>
+      <NextSeo
+        title="Zuyu"
+        description="We do things with AI."
+        openGraph={{
+          title: "Zuyu",
+          description: "We do things with AI.",
+          images: [
+            {
+              url: "/zuyu_og_image.png",
+              alt: "Zuyu logo"
+            },
+          ],
+          site_name: "Zuyu",
+        }}
+        twitter={{ cardType: "summary_large_image" }}
+      />
       <div className="center">
         <LogoAnimated width={150} height={150} fill="hsla(0,0%,100%,.9)" />
       </div>
